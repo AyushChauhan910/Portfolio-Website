2a03247acf742f3469901cc150442d7167ceb15b
AyushChauhan910 <talktoayushchauhan@gmail.com>
2025-06-14 22:06:01 +0530

    done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
+import pwebsite from 'C:/Coding/Projects/Portfolio Website/react-portfolio/src/pwebsite.png'
+import mrs from 'C:/Coding/Projects/Portfolio Website/react-portfolio/src/mrs.png'
+import nprd from 'C:/Coding/Projects/Portfolio Website/react-portfolio/src/nprd.png'
 
 const projects = [
   {
     title: "Music Recommendation System",
     description: "A machine learning project that recommends songs to users based on their listening history and preferences. Features data cleaning, feature engineering, and both content-based and collaborative filtering algorithms.",
     tech: ["Python", "Jupyter Notebook", "pandas", "scikit-learn", "matplotlib"],
-    image: "https://via.placeholder.com/300x180?text=Weather+App",
+    image: mrs,
     demo: "https://nbviewer.org/github/AyushChauhan910/ML_Project---Music_Recommendation_System/blob/main/Music_recommendation_system.ipynb",
     code: "https://github.com/AyushChauhan910/ML_Project---Music_Recommendation_System"
   },
@@ -15,7 +18,7 @@ const projects = [
     title: "Portfolio Website",
     description: "A personal portfolio website showcasing my skills, projects, and experience as a developer. Built with React and SCSS, featuring responsive design and animated UI",
     tech: ["React", "JavaScript", "SCSS", "HTML5", "Responsive Design"],
-    image: "https://via.placeholder.com/300x180?text=Portfolio+Site",
+    image: pwebsite,
     demo: "https://portfoliowebsite-three-lovat.vercel.app/",
     code: "https://github.com/AyushChauhan910/Portfolio-Website"
   },
@@ -23,7 +26,7 @@ const projects = [
     title: "NOVA Pharmacy Relational Database",
     description: "A comprehensive SQL database project designed for “NOVA”, a chain of pharmacies. The schema models patients, doctors, pharmaceutical companies, drugs, pharmacies, prescriptions, and contracts, supporting complex relationships and real-world pharmacy operations. To see the live demo , click on the button and copy the code from the github repo.",
     tech: ["SQL", "Relational Database Design"],
-    image: "https://via.placeholder.com/300x180?text=Chat+App",
+    image: nprd,
     demo: "https://livesql.oracle.com/next/",
     code: "https://github.com/AyushChauhan910/NOVA-pharmacy-relational-database"
   },
